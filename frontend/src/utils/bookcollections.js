766ced9891127330415e52534e2ea02099f47fe5
sofimes <sofanitmesfin19@gmail.com>
2025-10-10 16:32:01 +0300

    add bookcollection.js file

---
+++ frontend/src/utils/bookcollections.js
@@ -0,0 +1,32 @@
+export const bookcollections = [
+  {
+    id: 1,
+    title: "Adventure",
+    img: "/image/book2.jpg",
+    color: "bg-teal-800",
+  },
+  {
+    id: 2,
+    title: "classics",
+    img: "/image/book3.jpg",
+    color: "bg-gray-800",
+  },
+  {
+    id: 3,
+    title: "Detective ",
+    img: "",
+    color: "bg-orange-500",
+  },
+  {
+    id: 4,
+    title: "Bestseller",
+    img: "/image/book1.jpg",
+    color: "bg-indigo-800",
+  },
+  {
+    id: 5,
+    title: "Biographies",
+    img: "/image/book1.jpg",
+    color: "bg-gray-100",
+  },
+];
