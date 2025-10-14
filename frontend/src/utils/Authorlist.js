8a33e35324b0932691901e78425b37d202294f52
sofimes <sofanitmesfin19@gmail.com>
2025-10-14 13:27:46 +0300

    authorlist

---
+++ frontend/src/utils/Authorlist.js
@@ -0,0 +1,34 @@
+export const Authorlist = [
+  {
+    id: 1,
+    name: "Adam Grant",
+    career: "Psychlogist",
+    image: "",
+    lnurl: "",
+    igurl: "",
+  },
+  {
+    id: 2,
+    name: "Adam Grant",
+    career: "Psychlogist",
+    image: "",
+    lnurl: "",
+    igurl: "",
+  },
+  {
+    id: 3,
+    name: "Adam Grant",
+    career: "Psychlogist",
+    image: "",
+    lnurl: "",
+    igurl: "",
+  },
+  {
+    id: 4,
+    name: "Adam Grant",
+    career: "Psychlogist",
+    image: "/image/book2.jpg",
+    lnurl: "",
+    igurl: "",
+  },
+];
