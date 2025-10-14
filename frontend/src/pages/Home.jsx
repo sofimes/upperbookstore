222668a2d1486cdda118a9aadd368414f803c8f3
sofimes <sofanitmesfin19@gmail.com>
2025-10-14 13:26:54 +0300

    add author

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Main from "../components/home/Main";
 import BookCollection from "../components/home/BookCollection";
+import Authors from "../components/home/Authors";
 
 const Home = () => {
   return (
     <div>
       <Main />
       <BookCollection />
+      <Authors />
     </div>
   );
 };
