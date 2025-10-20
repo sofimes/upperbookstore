7c16833ef432569056cf2e27e4fbca3d0e328516
sofimes <sofanitmesfin19@gmail.com>
2025-10-20 23:56:54 +0300

    add database in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const { PORT } = require("./constants/env");
-
+const { connectDB } = require("./config/db");
 const app = express();
+
+app.use;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
