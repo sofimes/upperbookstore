e8728bb2d9bd7a264055124e8b73369fa8369909
sofimes <sofanitmesfin19@gmail.com>
2025-10-23 11:03:57 +0300

    generae and verify token

---
+++ backend/utils/token.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const {
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+} = require("../constants/env");
+
+const generateToken = (data) => {
+  const accessToken = jwt.sign(data, ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign(data, REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+  return { accessToken, refreshToken };
+};
+const verifyAndRefreshToken = (refreshToken) => {
+  return new Promise(
+    (refreshToken,
+    REFRESH_TOKEN_SECRET,
+    {},
+    (err, userId) => {
+      if (err || typeof userId === "undefined")
+        return reject(err || new Error("Invalid payload"));
+      const newAccessToken = jwt.sign({ id: userId }, ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m",
+      });
+      resolve({ accessToken: newAccessToken });
+    })
+  );
+};
+const decodeToken = (accessToken) =>
+  jwt.decode(accessToken, {
+    json: true,
+  });
+
+module.exports = { generateToken, verifyAndRefreshToken, decodeToken };
