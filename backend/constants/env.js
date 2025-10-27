929fe3eedc028bcc1c400d14dd2f41d690829891
sofimes <sofanitmesfin19@gmail.com>
2025-10-27 20:11:53 +0300

    env variables

--- backend/constants/env.js
+++ backend/constants/env.js
@@ -2,14 +2,14 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || "5500";
 const MONGO_URI = process.env.MONGO_URI;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL;
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
-
+const NODE_ENV = process.env.NODE_ENV || "development";
 module.exports = {
   PORT,
   MONGO_URI,
@@ -18,4 +18,5 @@ module.exports = {
   GOOGLE_CLIENT_SECRET,
   ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET,
+  NODE_ENV,
 };
