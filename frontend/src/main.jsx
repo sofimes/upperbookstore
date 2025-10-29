8147addf61997eb1880c17b90b2b6b159930ad1a
sofimes <sofanitmesfin19@gmail.com>
2025-10-29 23:45:01 +0300

    auth provider added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ThemeProvider } from "./context/Themecontext.jsx";
+import { AuthProvider } from "./context/AuthContext";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
+    <AuthProvider>
       <App />
-    </ThemeProvider>
+    </AuthProvider>
   </StrictMode>
 );
