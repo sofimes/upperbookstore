bf935f7baa59b9bdfc8b603268b459ac30d25029
sofimes <sofanitmesfin19@gmail.com>
2025-10-30 00:02:02 +0300

    change default

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -19,8 +19,8 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["admin", "user"],
-      default: "user",
+      enum: ["admin", "reader"],
+      default: "reader",
     },
   },
   {
