ff28010107cebdbcd038747792ef866696791f8d
sofimes <sofanitmesfin19@gmail.com>
2025-11-19 00:08:59 +0300

    book in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Main from "../components/home/Main";
 import BookCollection from "../components/home/BookCollection";
 import Authors from "../components/home/Authors";
+import Book from "../components/home/Book";
 
 const Home = () => {
   return (
     <div>
       <Main />
+      <Book />
       <BookCollection />
       <Authors />
     </div>
