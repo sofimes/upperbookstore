04d09d4dd58dba75f43c2b1d870caaddbaff097c
sofimes <sofanitmesfin19@gmail.com>
2025-11-18 23:08:10 +0300

    book route is done

--- backend/routes/book.route.js
+++ backend/routes/book.route.js
@@ -3,6 +3,7 @@ const { requireLogin } = require("../middleware/Auth.middleware");
 
 const {
   getBookController,
+  getBooksController,
   getBooksByGenreController,
   addBookController,
   updateBookController,
@@ -13,9 +14,11 @@ const {
 const bookRoute = Router();
 
 bookRoute.get("/getbook", requireLogin, getBookController);
+bookRoute.get("/allbooks", requireLogin, getBooksController);
 bookRoute.get("/booksbygen", requireLogin, getBooksByGenreController);
 bookRoute.post("/postbook", requireLogin, addBookController);
-bookRoute.put("/getbook", requireLogin, updateBookController);
+bookRoute.put("/updatebook", requireLogin, updateBookController);
 bookRoute.delete("/deletebook", requireLogin, deleteBookController);
+bookRoute.post("/ratebook", requireLogin, rateBookController);
 
 module.exports = bookRoute;
