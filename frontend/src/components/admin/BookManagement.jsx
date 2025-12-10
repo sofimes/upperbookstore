1eb52d4245f4392e204c2e95368c97336c66b1d6
sofimes <sofanitmesfin19@gmail.com>
2025-12-10 11:50:51 +0300

    book manage

--- frontend/src/components/admin/BookManagement.jsx
+++ frontend/src/components/admin/BookManagement.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BookManagement = () => {
-  return <div>BookManagement</div>;
+  return <div>BookManagement main part</div>;
 };
 
 export default BookManagement;
